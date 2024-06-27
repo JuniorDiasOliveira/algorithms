96d09199de78eebd2c2b9bf31dd7eb6a711c185f
Diogo Soares <vzdiogo@gmail.com>
2024-06-27 10:23:09 +0100

    mend

--- diogo/invertString/index.spec.ts
+++ diogo/invertString/index.spec.ts
@@ -2,38 +2,35 @@
 
 import invertString from "./index";
 
-describe("invertString tests", function () {
-  it("should return true", function () {
+describe("general test", () =>{
+  it("should return true", () => {
     expect(true).toBe(true);
   });
-});
 
-describe("invertString tests", function () {
-  it("should return the string inverted", function () {
+  it("should return the hello word inverted", () => {
     const input = "hello";
     const expectedOutput = "olleh";
     expect(invertString(input)).toBe(expectedOutput);
   });
-});
 
-describe("invertString tests", function () {
-  it("should return the string inverted", function () {
+
+  it("should return the OpenAI inverted", () => {
     const input = "OpenAI";
     const expectedOutput = "IAnepO";
     expect(invertString(input)).toBe(expectedOutput);
   });
-});
 
-describe("invertString tests", function () {
-  it("should return the string inverted", function () {
+
+
+  it("should return the string inverted", () => {
     const input = "Invert this string!";
     const expectedOutput = "!gnirts siht trevnI";
     expect(invertString(input)).toBe(expectedOutput);
   });
-});
 
-describe("invertString tests", function () {
-  it("should return the string inverted", function () {
+
+
+  it("should return the special characters inverted", function () {
     const input = "I º ~ ç";
     const expectedOutput = "ç ~ º I";
     expect(invertString(input)).toBe(expectedOutput);
